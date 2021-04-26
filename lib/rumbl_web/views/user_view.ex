787ac74f94df57587885916925f428b652b4ecee
trucopa <roqejr1307@gmail.com>
2021-04-25 22:27:31 -0300

    Rumbl project - part 1 chapter 5

---
+++ lib/rumbl_web/views/user_view.ex
@@ -0,0 +1,11 @@
+defmodule RumblWeb.UserView do
+  use RumblWeb, :view
+  alias RumblWeb.User
+
+  def first_name(%User{name: name}) do
+    name
+    |> String.split(" ")
+    |> Enum.at(0)
+  end
+
+end
